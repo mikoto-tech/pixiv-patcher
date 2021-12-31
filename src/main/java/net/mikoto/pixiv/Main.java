3f0c8c944e077f037c4d995714c81dee253a631a
mikoto <mikoto@ungine.cn>
2022-01-01 02:05:49 +0800

    修改版本号

--- src/main/java/net/mikoto/pixiv/Main.java
+++ src/main/java/net/mikoto/pixiv/Main.java
@@ -24,7 +24,7 @@ public class Main {
     /**
      * 常量
      */
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.0.1";
     public static final String DESCRIPTION = "Pixiv main project.";
     public static final String AUTHOR = "mikoto";
     public static final String PACKAGE = "net.mikoto.pixiv";
