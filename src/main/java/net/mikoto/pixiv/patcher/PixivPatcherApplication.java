67cacd0d7bd705c36876ebe7e1eabfb9cfef8846
mikoto <mikoto@ungine.cn>
2022-03-27 02:50:33 +0800

    [init](pixiv-patcher): init repo

---
+++ src/main/java/net/mikoto/pixiv/patcher/PixivPatcherApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.pixiv.patcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivPatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixivPatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/pixiv/patcher/PixivPatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.pixiv.patcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivPatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
